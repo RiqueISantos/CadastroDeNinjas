980f4a0e69597df897e180b6c6dbc9563ef03371
RiqueISantos <riquesantosoliveira@gmail.com>
2025-03-04 21:20:11 -0300

    build: Conecta o banco de dados H2 na minha aplicação Spring

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -19,7 +19,7 @@ public class MissoesModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String missao;
+    private String nome;
 
     private String dificuldade;
 
